c7a2e14988cadcd25e35cf82e8c3c8c466a17b37
Olivia <oliviaflexx@gmail.com>
2021-09-15 18:11:00 -0400

    finished contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,40 @@
 import React from "react";
+import linkedin from "./linkedin.svg";
+import github from "./github.svg";
+import email from "./email.svg";
 
 function Contact(props) {
   return (
-    <div>
-      <h1>Contact</h1>
-    </div>
+    <section className="main">
+      <h1 className="title">Contact</h1>
+      <div className="pink-in-button contact">
+        <h4>
+          I’m open to working at internships, freelancing or working
+          collaboratively with other developers
+        </h4>
+        <a
+          href="https://linkedin.com/in/olivia-felix-b03586205"
+          className="pink-out-button contact-link"
+        >
+          LinkedIn
+          <img id="linkedin" src={linkedin} alt="linkedin logo"></img>
+        </a>
+        <a
+          href="https://github.com/oliviaflexx"
+          className="pink-out-button contact-link"
+        >
+          GitHub
+          <img id="github" src={github} alt="github logo"></img>
+        </a>
+        <a
+          href="mailto:oliviaflexx@gmail.com"
+          className="pink-out-button contact-link"
+        >
+          Email
+          <img id="email" src={email} alt="email logo"></img>
+        </a>
+      </div>
+    </section>
   );
 }
 
