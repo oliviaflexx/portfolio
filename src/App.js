940c8f50854ec100d1ccb28a0c650003f40e49f4
Olivia <oliviaflexx@gmail.com>
2021-09-17 15:10:19 -0400

    added footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import Navbar from "./components/Navbar.js"; 
+import React, { useState, useEffect } from "react";
 import ReactRouter from "./components/ReactRouter"; 
 
 import './App.css';
 
 function App() {
+  const [isLoading, setLoading] = useState(true);
+
+  function fakeRequest() {
+    return new Promise((resolve) => setTimeout(() => resolve(), 2000));
+  }
+
+  useEffect(() => {
+    fakeRequest().then(() => {
+      const el = document.querySelector(".loader-container");
+      if (el) {
+        el.remove();
+        setLoading(!isLoading);
+      }
+    });
+  }, [isLoading]);
+
+  if (isLoading) {
+    return null;
+  }
   return (
     <main className="app">
       <ReactRouter />
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,51 @@
 import React from "react";
+import { motion } from 'framer-motion';
+import { v4 as uuid } from 'uuid';
 
 function Home(props) {
   return (
     <section className="main">
       <div className="row">
-        <h2 className="home">Hi, my name is</h2>
-        <h1 className="home">Olivia Felix</h1>
+        <motion.h2
+          className="home"
+          initial={{ x: -650, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{
+            type: "tween",
+            duration: "2",
+            delay: "1",
+          }}
+        >
+          Hi, my name is
+        </motion.h2>
+        <motion.h1
+          className="home"
+          initial={{ scale: 0.1, opacity: 0 }}
+          animate={{ scale: 1, opacity: 1 }}
+          transition={{
+            type: "tween",
+            duration: "2",
+            delay: "1",
+          }}
+        >
+          Olivia Felix
+        </motion.h1>
       </div>
       <div className="row home">
-        <div className="col-12 col-sm-6 black-in-button">
+        <motion.div
+          className="col-12 col-sm-6 black-in-button"
+          initial={{opacity: 0 }}
+          animate={{opacity: 1 }}
+          transition={{
+            type: "tween",
+            duration: "2",
+            delay: "2",
+          }}
+        >
           I love making websites and web applications.
           <br></br>
           Welcome to my portfolio!
-        </div>
+        </motion.div>
       </div>
     </section>
   );
