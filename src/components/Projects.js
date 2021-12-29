4cbb8f6cd12f685fd0a185eaafaf1d1b345b188d
Olivia <oliviaflexx@gmail.com>
2021-12-29 09:20:05 -0600

    deleted project page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,5 @@
 import React, { useState} from "react";
 import { data } from "../project_data";
-import { Link } from "react-router-dom";
 import github from "./github.svg";
 import live from "./live.svg";
 
@@ -64,35 +63,7 @@ function Projects(props) {
                 )}
               </div>
             </div>
-            // <div className="col-xl-4 col-lg-6 col-sm-12 projects">
-            //   <div key="{project.id}" className="project-card blue-in-button">
-            //     <Link
-            //       className="blue-out-button link"
-            //       to={`/project/${project.id}`}
-            //     >
-            //       {project.name}
-            //     </Link>
-            //     <div className="blue-in-button short-descrip">
-            //       {project.short_descrip}
-            //     </div>
-            //     <a
-            //       href={project.github}
-            //       className="blue-out-button logo-link left"
-            //       target="_blank"
-            //       rel="noopener noreferrer"
-            //     >
-            //       <img src={github} alt="github"></img>
-            //     </a>
-            //     <a
-            //       href={project.live}
-            //       className="blue-out-button logo-link right"
-            //       target="_blank"
-            //       rel="noopener noreferrer"
-            //     >
-            //       <img id="live" src={live} alt="live"></img>
-            //     </a>
-            //   </div>
-            // </div>
+   
           );
         })}
       </div>
